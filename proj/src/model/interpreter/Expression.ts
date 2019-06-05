92014b67ec29c9e44713f20f50ae56f24803c127
Nelson Almeida <up201505394@fe.up.pt>
2019-06-05 04:55:36 +0100

    finish super expression class

--- proj/src/model/interpreter/Expression.ts
+++ proj/src/model/interpreter/Expression.ts
@@ -1,10 +1,52 @@
 import { Command } from '../commands/Command';
+import { CreateShapeExpression } from './CreateShapeExpression';
+import { TranslateExpression } from './TranslateExpression';
+import { ScaleExpression } from './ScaleExpression';
+import { DrawExpression } from './DrawExpression';
 
 export class Expression {
     protected errors: Array<string> = [];
     protected command: Command = null;
 
+    protected addError(error: string) {
+        this.errors.push(error);
+    }
+
+    protected setCommand(command: Command) {
+        this.command = command;
+    }
+
+    public getErrors(): Array<string> {
+        return this.errors;
+    }
+
+    public getCommand(): Command {
+        return this.command;
+    }
+
     public interpret(context: string): boolean {
-        return false;
+        let instruction: string = context.split(' ')[0];
+
+        let expression: Expression;
+        switch(instruction) {
+            case 'create':
+                expression = new CreateShapeExpression(this);
+                break;
+            case 'translate':
+                expression = new TranslateExpression(this);
+                break;
+            case 'scale':
+                expression = new ScaleExpression(this);
+                break;
+            case 'draw':
+                expression = new DrawExpression(this);
+                break;
+            default:
+                this.addError("There's no instruction called " + instruction);
+                return false;
+        }
+
+        let contextArguments = context.substr(context.indexOf(' ') + 1);
+        return expression.interpret(contextArguments);
     }
 }
